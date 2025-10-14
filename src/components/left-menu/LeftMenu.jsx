3856239a4e21abfea186e189d5d56b5c7f452fd2
Asadov-A <asatovjam@gmail.com>
2025-10-14 17:58:55 +0500

    start project

---
+++ src/components/left-menu/LeftMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import webLogo from '../../assets/icons/web-logo.png'
+import './LeftMenu.css'
+
+function LeftMenu() {
+  return (
+    <div className='left-menu'>
+
+    </div>
+  )
+}
+
+export default LeftMenu
---
+++ src/components/main/customers/customers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './customers.css'
+
+function customers() {
+  return (
+    <div className='customers-box'>
+
+    </div>
+  )
+}
+
+export default customers
