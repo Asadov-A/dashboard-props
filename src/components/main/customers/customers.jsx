f719b1789f986aa6549cb209bfe8645ca48226be
Asadov-A <asatovjam@gmail.com>
2025-10-16 16:09:02 +0500

    fix bags

--- src/components/main/customers/customers.jsx
+++ src/components/main/customers/customers.jsx
@@ -87,8 +87,8 @@ function Customers() {
               <option value="oldest">Oldest</option>
               <option value="active">Active</option>
               <option value="inactive">Inactive</option>
-              <option value="az">A-Z</option>
-              <option value="za">Z-A</option>
+              <option value="a-z">A-Z</option>
+              <option value="z-a">Z-A</option>
             </select>
           </button>
         </div>
