85654ed99c58e3c5ff5a28c839b078b795e771f7
Asadov-A <asatovjam@gmail.com>
2025-10-16 15:26:32 +0500

    customers-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Customers from './components/main/customers/customers'
+import LeftMenu from './components/left-menu/LeftMenu'
 
 function App() {
 
   return (
     <>
+
+
+  <main>
+     <LeftMenu></LeftMenu>
+
+     <Customers></Customers>
+  </main>
     </>
   )
 }
